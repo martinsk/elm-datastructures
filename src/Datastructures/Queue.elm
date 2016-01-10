807ed7eb1a96813a275fa72494fb403f3a80729c
Martin Kristiansen <msk@tigertext.com>
2016-01-10 13:33:24 -0800

    initial commit

---
+++ src/Datastructures/Queue.elm
@@ -0,0 +1,90 @@
+module Datastructures.Queue where
+
+{-| This Module implements a simple LIFO queue 
+
+# Definition
+@docs Queue
+
+This is based on the 
+
+# Fundamentals 
+@docs init, enqueue, dequeue, length
+
+# Usefull functions
+@docs foldr, foldl, map, fromList, toList
+
+-}
+
+{-| a simple queue.
+-}
+type alias Queue a = (List a, List a)
+
+{-| Creates an empty queue -}
+                   
+init : Queue a
+init = ([], [])
+
+
+{-|Enqueue an element on a queue -}
+enqueue :  a -> Queue a -> Queue a
+enqueue a (inqueue, outqueue) =
+  ((a::inqueue), outqueue)
+
+{-|Dequeues an element of the end of a queue, and also returns thel
+element -}
+
+dequeue : Queue a -> (Maybe a, Queue a)
+dequeue (inqueue,outqueue) =
+  case outqueue of
+    [] ->
+      case inqueue of
+        [] -> (Nothing,(inqueue, outqueue))
+
+        (_::_) -> dequeue([], List.reverse inqueue)
+                   
+    (x::xs) ->
+      (Just x, (inqueue, xs))
+
+
+{-| Get the length(number of elements) in the queue -}
+length : Queue a -> Int
+length (inqueue, outqueue) =
+  let
+    inqueue_len  = List.length inqueue
+    outqueue_len = List.length outqueue
+  in
+    inqueue_len + outqueue_len
+  
+{-| Fold across a queue front ot back -}
+
+foldr : ( a -> b -> b) -> b -> Queue a -> b
+foldr f acc (inqueue, outqueue) =
+  List.foldl f (List.foldr f acc inqueue) outqueue
+
+{-| Fold across a queue back ot front -}
+
+foldl : ( a -> b -> b) -> b -> Queue a -> b
+foldl f acc (inqueue, outqueue) =
+  List.foldr f (List.foldl f acc outqueue) inqueue
+
+
+{-| maps from a queue of type a to a queue containing elements of type
+b -}
+
+map : (a -> b) -> Queue a -> Queue b
+map f (inqueue, outqueue) =
+  (List.map f inqueue, List.map f outqueue)
+
+
+{-| converts a queue into a list  -}
+
+fromList : List a -> Queue a
+fromList l =
+  (l, [])
+
+  
+{-| converts a list into a queue  -}
+  
+toList : Queue a -> List a
+toList (inqueue, outqueue) =
+  inqueue ++ (List.reverse outqueue)
---
+++ test/QueueTest.elm
@@ -0,0 +1,151 @@
+module QueueTest where
+
+import Ajour.Queue as Queue
+
+import Check exposing (..)
+import Check.Investigator exposing (..)
+
+
+claim_order_maintained =
+  claim "order maintained"
+          `that`
+                 (\list -> Queue.toList (Queue.fromList (list)))
+          `is`
+               (\list -> list)
+          `for`
+                (list int)
+
+claim_order_maintained_enqueue =
+  claim "order maintained_enqueue"
+          `that`
+                 (\(list, e)->
+                   let
+                     queue = Queue.fromList list
+                   in
+                     Queue.toList (Queue.enqueue e queue)
+                 )
+          `is`
+               (\(list, e) -> e::list )
+          `for`
+                tuple ((list int), int)
+
+
+claim_order_maintained_dequeue =
+  claim "order maintained_dequeue"
+          `that`
+                 (\(list, e)->
+                   let
+                     queue = Queue.fromList list
+                     (_, queue') = (Queue.dequeue queue)
+                   in
+                     Queue.toList (queue')
+                 )
+          `is`
+               (\(list, e) -> let len = List.length list
+                                  list' = List.take (len - 1) list 
+                              in list')
+          `for`
+                tuple ((list int), int)
+
+                
+claim_operations_eguevalence =
+  claim "claim_operations_eguevalence"
+          `that`
+                 (\ops->
+                   let
+                     queue = Queue.init
+                     (_, _, steps) = List.foldr (\op (q, i, steps) ->
+                                         case op of
+                                           True ->
+                                             let q' = Queue.enqueue i q
+                                             in (q', i+1, (q' :: steps))
+                                           False ->
+                                             let (_, q') = Queue.dequeue q
+                                             in (q',i, (q' :: steps))
+                     
+                                         ) (queue, 0, [] ) ops
+                   in
+                     List.map Queue.toList steps
+                 )
+          `is`
+                 (\ops->
+                   let
+                     queue = []
+                     (_, _, steps) = List.foldr (\op (q, i, steps) ->
+                                         case op of
+                                           True ->
+                                             (i::q, i+1, ((i::q) :: steps))
+                                           False ->
+                                             let
+                                               q' = List.take ((List.length q) - 1) q
+                                             in (q',i, (q'::steps))
+                     
+                                         ) (queue, 0, []) ops
+                   in
+                     steps
+                 )
+
+          `for`
+                (list bool)
+
+
+claim_correct_length =
+  claim "claim_correct_length"
+          `that`
+                 (\ops->
+                   let
+                     queue = Queue.init
+                     (_, _, steps) = List.foldr (\op (q, i, steps) ->
+                                         case op of
+                                           True ->
+                                             let
+                                               q' = Queue.enqueue i q
+                                               l  = Queue.length q'
+                                             in (q', i+1, ((q',l) :: steps))
+                                           False ->
+                                             let
+                                               (_, q') = Queue.dequeue q
+                                               l  = Queue.length q'
+                                             in (q',i, ((q',l) :: steps))
+                     
+                                         ) (queue, 0, [] ) ops
+                   in
+                     List.map (\(q, l) -> (Queue.toList q, l))  steps
+                 )
+          `is`
+                 (\ops->
+                   let
+                     queue = []
+                     (_, _, steps) = List.foldr (\op (q, i, steps) ->
+                                         case op of
+                                           True ->
+                                             let q' = (i::q)
+                                                 l = List.length q'
+                                             in
+                                               (q', i+1, ((q', l) :: steps))
+                                           False ->
+                                             let
+                                               q' = List.take ((List.length q) - 1) q
+                                               l = List.length q'
+                                             in (q',i, ((q', l)::steps))
+                     
+                                         ) (queue, 0, []) ops
+                   in
+                     steps
+                 )
+
+          `for`
+                (list bool)
+
+                
+
+suite_reverse =
+  suite "Queue Suite"
+    [ claim_order_maintained
+    , claim_order_maintained_enqueue
+    , claim_order_maintained_dequeue
+    , claim_operations_eguevalence
+    , claim_correct_length]
+
+    
+result = quickCheck suite_reverse
